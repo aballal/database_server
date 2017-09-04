73a0c1d0f030b8291a632c074e29978658aceb11
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-04 14:13:02 +0100

    DataStore can set and get a key

--- app/models/data_store.rb
+++ app/models/data_store.rb
@@ -3,6 +3,14 @@ class DataStore
     @data_hash = Hash.new
   end
 
+  def set(key, value)
+    data_hash[key] = value
+  end
+
+  def get(key)
+    data_hash[key]
+  end
+
   def keys
     data_hash.keys
   end
--- spec/data_store_spec.rb
+++ spec/data_store_spec.rb
@@ -1,7 +1,14 @@
 describe DataStore do
   subject(:data_store) { described_class.new }
+  let(:key) { :name }
+  let(:value) { 'A Ballal' }
 
   it 'has no keys to begin with' do
     expect(data_store.keys).to be_empty
+  end
+
+  it 'can set and get a key' do
+    data_store.set(key, value)
+    expect(data_store.get(key)).to eq value
   end
 end
