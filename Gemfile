7e9b79837acbd8aedda55b735ede7145e94082b8
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-09-04 12:06:35 +0100

    Added launchy gem, changed port to 4000

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'sinatra'
 
 group :test do
   gem 'capybara'
+  gem 'launchy'
   gem 'rspec'
   gem 'rubocop'
 end
--- spec/features/visit_database_server_spec.rb
+++ spec/features/visit_database_server_spec.rb
@@ -1,6 +1,6 @@
 feature 'Visiting Database Server' do
-  scenario 'visiting local host on port 9292' do
-    visit 'http://localhost:9292/'
+  scenario 'visiting local host on port 4000' do
+    visit 'http://localhost:4000/'
     expect(page).to have_content 'Database Server Tech Test'
   end
 end
